3b199e75717453384d98363cbad680710531eb0e
Co-Ji <sungji226@gmail.com>
2022-07-07 00:36:33 +0900

    eslint, prettier 연습

--- src/Prac.tsx
+++ src/Prac.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
 
 const Prac = () => {
-    return <div>hello</div>;
+	const abc = 'abc';
+	const abcd = 'abcd';
+	const ab = {
+		ab: abc,
+		abcd: abcd,
+	};
+	const abcde = {
+		ab: abc,
+		abcd: abcd,
+	};
+	const fruit = ['apple', 'banana', 'grape'];
+
+	const eat = () => {
+		console.log(fruit);
+	};
+
+	console.log(abc, abcd, ab);
+
+	return <div>hello</div>;
 };
 
 export default Prac;
